33d3f8472d91ea4f12afc7f03bc9717ef0c2184c
Abhishek Srivastava <bits.abhi@gmail.com>
2026-02-10 09:15:52 +0530

    v3.5.2: README + friendly fallbacks

--- bazinga/cli.py
+++ bazinga/cli.py
@@ -91,7 +91,7 @@ class BAZINGA:
     Layer 4 only called when necessary.
     """
 
-    VERSION = "3.5.1"
+    VERSION = "3.5.2"
 
     def __init__(self, verbose: bool = False):
         self.verbose = verbose
