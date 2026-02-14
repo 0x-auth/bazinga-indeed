c89e16ccf91c151b5b79960dd981009d5f206ecc
Abhishek Srivastava <bits.abhi@gmail.com>
2026-02-14 16:17:35 +0530

    v4.8.10 - fix lazy import in --stats handler

--- bazinga/__init__.py
+++ bazinga/__init__.py
@@ -12,7 +12,7 @@ Usage:
 
 from .cli import BAZINGA, main_sync, main
 
-__version__ = "4.8.9"
+__version__ = "4.8.10"
 __all__ = ['BAZINGA', 'main_sync', 'main', '__version__']
 
 # New in v3.4.0: Quantum, ΛG, Tensor modules
